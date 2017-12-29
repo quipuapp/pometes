d7d58f26ecaa02dc0f6b7c64e47582f21bd8c51b
Pau Compte <pausandalio@gmail.com>
2017-12-29 12:42:25 +0100

    Add methods to class Pometes

--- lib/pometes.rb
+++ lib/pometes.rb
@@ -1,4 +1,27 @@
 class Pometes
+  def verse(number)
+    case number
+    when 0
+      "Si miereu el vent d'on ve, veureu el pomer com dansa,\n" +
+      "si mireu el vent d'on ve, veureu com dansa el pomer.\n"
+    when 1
+      "1 pometa té el pomer, d'1 una, d'1 una,\n" +
+      "1 pometa té el pomer, d'1 una li'n caigué.\n"
+    else
+      "#{number} pometes té el pomer, de #{number} una, de #{number} una,\n" +
+      "#{number} pometes té el pomer, de #{number} una li'n caigué.\n"
+    end
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).map do |i|
+      verse(i)
+    end.join("\n" + verse(0) + "\n")
+  end
+
+  def song
+    verses(10, 1) + "\n#{verse(0)}"
+  end
 end
 
 ## If you want to see the song in your console uncomment the next line and
